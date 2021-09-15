0df1bd857cc124a08754f69ac5c4db2df5a0c745
brucemoran <bruce01campus@gmail.com>
2021-09-16 00:25:17 +0100

    test runs

--- DNN-Model/test/example_run.sh
+++ DNN-Model/test/example_run.sh
@@ -1,6 +1,7 @@
 #! bash
 
+perl pcawg_example_join.pl
 mkdir -p /TumorType-WGS/DNN-Model/test/test_output
 python3 /TumorType-WGS/DNN-Model/predict_cancer.py \
-  --input /TumorType-WGS/DNN-Model/test/input_example.csv \
+  --input /TumorType-WGS/DNN-Model/test/example_input.csv \
   --output "/TumorType-WGS/DNN-Model/test/test_output"
