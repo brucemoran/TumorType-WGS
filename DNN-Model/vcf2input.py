363896b3d562bbc6a583724b3564b2b9f5b075ba
brucemoran <bruce01campus@gmail.com>
2021-09-30 23:54:47 +0100

    vcf2input.py

--- DNN-Model/vcf2input.py
+++ DNN-Model/vcf2input.py
@@ -46,7 +46,9 @@ def reverse_complement(seq):
 def fasta_reader(fastafile):
     seq_list = {}
     for index, record in enumerate(SeqIO.parse(fastafile, "fasta")):
-        seq_list[record.name] = record.seq
+        unw = ["X", "Y", "MT", "_", "GL", "KI"]
+        if not any([ss in record.name for ss in unw]):
+            seq_list[record.name] = record.seq
     return(seq_list)
 
 
@@ -56,25 +58,57 @@ def vcf2df(filename, seq_list):
     # count only SNVs
     df = allel.vcf_to_dataframe(filename, fields='*', alt_number=2)
     print("--- %s Total Variants in the VCF file ---" % len(df))
-    chr_list = dict.keys(seq_list)
+    ##test for chr on seq_list, then add if not found in VCF or vice-versa
+    seq_list_chr = "TRUE"
+    try:
+        seq_list["chr1"]
+    except KeyError:
+        seq_list_chr = "FALSE"
+    chr_list = list(dict.keys(seq_list))
     if df['CHROM'].iloc[0].find("chr") < 0:
-        df['CHROM'] = "chr" + df['CHROM']
+        if seq_list_chr == "FALSE":
+            print("Fasta and VCF have no \'chr\' prefix...")
+        else:
+            print("Fasta has \'chr\' prefix, but VCF doesn't, fixing...")
+            df['CHROM'] = "chr" + df['CHROM']
+    else:
+        if seq_list_chr == "FALSE":
+            print("Fasta has no \'chr\' prefix, but VCF does, fixing...")
+            df['CHROM'] = df['CHROM'].str.replace("chr", "")
     vcf_df = df[(df['is_snp'] == True) & (df['CHROM'].isin(chr_list) == True)]
     print("--- %s SNVs in the VCF file ---" % len(vcf_df))
     return(vcf_df)
 
 
-def vcf2bins(tmp1, sample_name):
+def vcf2bins(tmp1, seq_list, sample_name):
     # set up data frame for SNV counts
     # load header
-    filename = '/TumorType-WGS/DNN-Model/.1Mb.header.gz'
-    binhd_df = pd.read_csv(filename, compression='gzip', header=None)
+    ##set up bins from fasta seq_list set
+    dkeys = list(dict.keys(seq_list))
+    nochr_dkeys = [k.replace("chr", "") for k in dkeys]
+    chr_bases = {}
+    total_bases = 0
+    seq_str = list(map(str, sorted(list(map(int, nochr_dkeys)))))
+    if dkeys[0].find("chr") == 0:
+        seq_str = ["chr" + k for k in seq_str]
+    for i in seq_str:
+        total_bases += len(seq_list[i])
+        chr_bases[i] = len(seq_list[i])
+    bin_bases = round(total_bases/2896.5)
+    bases_df = pd.Series(chr_bases).apply(lambda x: int(round(float(x) / bin_bases)))
+    binhd_tmp = {}
+    a = 0
+    for i in seq_str:
+        for j in range(0,bases_df[i]):
+            binhd_tmp[a] = str(i) + "." + str(j)
+            a += 1
+    binhd_df = pd.DataFrame(pd.Series(binhd_tmp))
     # initial dataframe
     test = tmp1.CHROM + '.' + \
-        tmp1.POS.apply(lambda x: int(round(float(x) / 1000000))).astype(str)
+        tmp1.POS.apply(lambda x: int(round(float(x) / bin_bases))).astype(str)
     bindf_tmp = pd.DataFrame(
         {"bins": pd.Series(pd.Categorical(test, categories=binhd_df.iloc[:, 0]))})
-    bins_df = bindf_tmp.groupby('bins').size().reset_index(name=sample_name)
+    bins_df = bindf_tmp.groupby('bins').size().reset_index(name = sample_name)
     # cap max bin count as 50
     tmp = bins_df.set_index("bins").to_numpy()
     tmp = np.clip(tmp, 0, 50)
@@ -88,28 +122,24 @@ def df2mut(tmp1, seq_list, sample_name):
     # load header
     mut_df_header = pd.read_csv(
         '/TumorType-WGS/DNN-Model/Mut-Type-Header.csv')
-    # initial dataframe
+        # initial dataframe
     changes = []
     ref_vcf = []
     ref_grch = []
-    tmp1 = tmp1.reset_index()
-    tmp1 = tmp1.to_numpy()
-    for i in range(len(tmp1)):
-        ref = tmp1[i, 4]
-        ch = tmp1[i, 1]
-        st = tmp1[i, 2]
-        alt = tmp1[i, 5]
+    tmp2 = tmp1.reset_index()
+    tmp3 = tmp2.to_numpy()
+    for i in range(len(tmp3)):
+        ref = tmp3[i, 4]
+        ch = tmp3[i, 1]
+        st = tmp3[i, 2]
+        alt = tmp3[i, 5]
         ref_vcf.append(ref)
         ref_base = seq_list[ch][st - 1].upper()
         ref_grch.append(ref_base)
         ref_cont = seq_list[ch][st - 2:st + 1].upper()
         if(not ref == ref_base):
-            print("Something wrong with the Genome Version, reference bases from VCF file doesn't match with records on hg19 genome\n")
-            print(ch)
-            print(st)
-            print(ref)
-            print(ref_base)
-            print(ref_cont)
+            print("Mismatch in VCF and fasta genome versions, please ensure they are the same\n")
+            print(ch, ":", st, "_", ref, "-", ref_base, "..", ref_cont)
         if (re.search('[GT]', ref)):
             ref = reverse_complement(ref)
             ref_cont = reverse_complement(ref_cont)
@@ -140,18 +170,18 @@ def df2mut(tmp1, seq_list, sample_name):
 
 if __name__ == '__main__':
     # load FASTA Files:
-    start_time1 = time.time(args.fasta)
+    start_time1 = time.time()
     seq_list = fasta_reader(args.fasta)
     print("--- %s seconds to load %s FASTA Files---" %
-          ((time.time() - start_time1), len(seq_list)))
+      ((time.time() - start_time1), len(seq_list)))
 
     # load VCF file:
-    vcf_df = vcf2df(args.vcf)
+    vcf_df = vcf2df(args.vcf, seq_list)
     sample_name = args.sampleID
 
     # convert VCF to Bin counts
     start_time = time.time()
-    bins_df = vcf2bins(vcf_df, sample_name)
+    bins_df = vcf2bins(vcf_df, seq_list, sample_name)
     print(bins_df.shape)
     print("--- %s seconds to make SNV Counts Data Frame ---" %
           (time.time() - start_time))
@@ -168,6 +198,6 @@ if __name__ == '__main__':
     cb_df = cb_df.set_index('bins')
 
     cb_df = cb_df.transpose()
-    cb_df.to_csv(args.sampleID + '.predict_cancer_input.csv', header=True)
+    cb_df.to_csv(sample_name + '.predict_cancer_input.csv', header=True)
 
     print("total --- %s seconds ---" % (time.time() - start_time1))
