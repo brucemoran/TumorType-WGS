df91e13b5c373ebc4869f751f3053b2bc5fa564e
brucemoran <bruce01campus@gmail.com>
2021-10-04 15:00:18 +0100

    main

--- DNN-Model/predict_cancer.py
+++ DNN-Model/predict_cancer.py
@@ -43,63 +43,63 @@ from keras import regularizers
 
 if __name__== "__main__":
 
-parser = argparse.ArgumentParser(description='Predict cancer type from mutation topology and mutation types', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--input_csv', help='Input (use \'vcf2input.py\')', required = True)
-parser.add_argument('--output_dir', help='Path to directory in which to write output')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Predict cancer type from mutation topology and mutation types', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input_csv', help='Input (use \'vcf2input.py\')', required = True)
+    parser.add_argument('--output_dir', help='Path to directory in which to write output')
+    args = parser.parse_args()
 
-if args.output_dir == None:
-    args.output_dir = "./"
+    if args.output_dir == None:
+        args.output_dir = "./"
 
-d = pd.read_csv('/TumorType-WGS/DNN-Model/fold1_d.csv')
-#factor = d.Factor
-#cancer = d.Cancer
-#factor_dict = dict(zip(factor, cancer))
-	factor_dict = {0: 'Bone-Osteosarc',
- 1: 'Breast-AdenoCA',
- 2: 'CNS-GBM',
- 3: 'CNS-Medullo',
- 4: 'CNS-PiloAstro',
- 5: 'ColoRect-AdenoCA',
- 6: 'Eso-AdenoCA',
- 7: 'Head-SCC',
- 8: 'Kidney-ChRCC',
- 9: 'Kidney-RCC',
- 10: 'Liver-HCC',
- 11: 'Lung-AdenoCA',
- 12: 'Lung-SCC',
- 13: 'Lymph-BNHL',
- 14: 'Lymph-CLL',
- 15: 'Myeloid-MPN',
- 16: 'Ovary-AdenoCA',
- 17: 'Panc-AdenoCA',
- 18: 'Panc-Endocrine',
- 19: 'Prost-AdenoCA',
- 20: 'Skin-Melanoma',
- 21: 'Stomach-AdenoCA',
- 22: 'Thy-AdenoCA',
- 23: 'Uterus-AdenoCA'}
-    ##cat keras model.a* files and use that as model
-    # filenames = ['model/ensemble_model.keras.aa', 'model/ensemble_model.keras.ab', 'model/ensemble_model.keras.ac', 'model/ensemble_model.keras.ad']
-    # with open('ensemble_model.keras', 'w') as outfile:
-    #     for fname in filenames:
-    #         with open(fname) as infile:
-    #             for line in infile:
-    #                 outfile.write(line)
-os.system("cat /TumorType-WGS/DNN-Model/model/ensemble_model.keras.* >> /TumorType-WGS/DNN-Model/model/ensemble_model.keras")
-model = load_model('/TumorType-WGS/DNN-Model/model/ensemble_model.keras')
-input_file = args.input_csv
-output_dir = args.output_dir
-output_name = input_file.split(".")[0]
-data = pd.read_csv(input_file, index_col = [0])
-x_input = data.values
-if x_input.shape[-1] != 3047:
-	print('Input file not of correct dimensionality. See README for properly formatted file')
-predictions = model.predict(x_input)
-class_predictions = np.argmax(predictions, axis = 1)
-### make predictions dataframe
-predictions_df = pd.DataFrame(data = predictions, index = data.index, columns = d.Cancer)
-predictions_df.to_csv(output_dir + "/" + output_name + ".cancer_prediction_probability.tsv", sep = '\t')
-cancer_classes = [factor_dict[i] for i in class_predictions]
-class_df = pd.DataFrame({'cancer_prediction':cancer_classes}, index = data.index)
-class_df.to_csv(output_dir + "/" + output_name + "cancer_predictions.tsv", sep = '\t')
+    d = pd.read_csv('/TumorType-WGS/DNN-Model/fold1_d.csv')
+    #factor = d.Factor
+    #cancer = d.Cancer
+    #factor_dict = dict(zip(factor, cancer))
+    	factor_dict = {0: 'Bone-Osteosarc',
+     1: 'Breast-AdenoCA',
+     2: 'CNS-GBM',
+     3: 'CNS-Medullo',
+     4: 'CNS-PiloAstro',
+     5: 'ColoRect-AdenoCA',
+     6: 'Eso-AdenoCA',
+     7: 'Head-SCC',
+     8: 'Kidney-ChRCC',
+     9: 'Kidney-RCC',
+     10: 'Liver-HCC',
+     11: 'Lung-AdenoCA',
+     12: 'Lung-SCC',
+     13: 'Lymph-BNHL',
+     14: 'Lymph-CLL',
+     15: 'Myeloid-MPN',
+     16: 'Ovary-AdenoCA',
+     17: 'Panc-AdenoCA',
+     18: 'Panc-Endocrine',
+     19: 'Prost-AdenoCA',
+     20: 'Skin-Melanoma',
+     21: 'Stomach-AdenoCA',
+     22: 'Thy-AdenoCA',
+     23: 'Uterus-AdenoCA'}
+        ##cat keras model.a* files and use that as model
+        # filenames = ['model/ensemble_model.keras.aa', 'model/ensemble_model.keras.ab', 'model/ensemble_model.keras.ac', 'model/ensemble_model.keras.ad']
+        # with open('ensemble_model.keras', 'w') as outfile:
+        #     for fname in filenames:
+        #         with open(fname) as infile:
+        #             for line in infile:
+        #                 outfile.write(line)
+    os.system("cat /TumorType-WGS/DNN-Model/model/ensemble_model.keras.* >> /TumorType-WGS/DNN-Model/model/ensemble_model.keras")
+    model = load_model('/TumorType-WGS/DNN-Model/model/ensemble_model.keras')
+    input_file = args.input_csv
+    output_dir = args.output_dir
+    output_name = input_file.split(".")[0]
+    data = pd.read_csv(input_file, index_col = [0])
+    x_input = data.values
+    if x_input.shape[-1] != 3047:
+    	print('Input file not of correct dimensionality. See README for properly formatted file')
+    predictions = model.predict(x_input)
+    class_predictions = np.argmax(predictions, axis = 1)
+    ### make predictions dataframe
+    predictions_df = pd.DataFrame(data = predictions, index = data.index, columns = d.Cancer)
+    predictions_df.to_csv(output_dir + "/" + output_name + ".cancer_prediction_probability.tsv", sep = '\t')
+    cancer_classes = [factor_dict[i] for i in class_predictions]
+    class_df = pd.DataFrame({'cancer_prediction':cancer_classes}, index = data.index)
+    class_df.to_csv(output_dir + "/" + output_name + "cancer_predictions.tsv", sep = '\t')
